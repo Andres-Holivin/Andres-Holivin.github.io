85c3ec531346f491c91a542d4a88da9e01a2d1e9
andres holivin <andres12holivin@gmail.com>
2022-12-20 07:06:50 +0000

    Update ProjectPage.js

--- pages/components/ProjectPage.js
+++ pages/components/ProjectPage.js
@@ -6,19 +6,61 @@ import {
   Text,
   Image,
   Icon,
-  IconButton
+  IconButton,
 } from "@chakra-ui/react";
 import { FaGlobeAsia, FaGithub } from "react-icons/fa";
 import { IoLogoGooglePlaystore } from "react-icons/io5";
 
 const ProjectPage = () => {
   const icons = [
-    {'id':1,icons:[<Icon key={1} as={FaGlobeAsia} />,<Icon  key={1} as={FaGithub} />,<Icon  key={1} as={IoLogoGooglePlaystore} />]},
-    {'id':2,icons:[<Icon key={1} as={FaGlobeAsia} />,<Icon  key={1} as={FaGithub} />,<Icon  key={1} as={IoLogoGooglePlaystore} />]},
-    {'id':3,icons:[<Icon key={1} as={FaGlobeAsia} />,<Icon  key={1} as={FaGithub} />,<Icon  key={1} as={IoLogoGooglePlaystore} />]},
+    {
+      id: 1,
+      title: "ToList",
+      desc: "Desc1",
+      icons: [
+        <Icon key={1} as={FaGlobeAsia} />,
+        <Icon key={1} as={FaGithub} />,
+        <Icon key={1} as={IoLogoGooglePlaystore} />,
+      ],
+    },
+    {
+      id: 2,
+      title: "hello2",
+      desc: "Desc2",
+      icons: [
+        <Icon key={1} as={FaGlobeAsia} />,
+        <Icon key={1} as={FaGithub} />,
+        <Icon key={1} as={IoLogoGooglePlaystore} />,
+      ],
+    },
+    {
+      id: 3,
+      title: "hello3",
+      desc: "Desc3",
+      icons: [
+        {
+          link: "https://appgallery.huawei.com/app/C105260341",
+          icon: <Icon key={1} as={FaGlobeAsia} />,
+        },
+        {
+          link: "https://appgallery.huawei.com/app/C105260341",
+          icon: <Icon key={1} as={FaGithub} />,
+        },
+        {
+          link: "https://appgallery.huawei.com/app/C105260341",
+          icon: <Icon key={1} as={IoLogoGooglePlaystore} />,
+        },
+      ],
+    },
   ];
   return (
-    <Box id="project" minH="100vh" backgroundColor="" paddingTop="80px">
+    <Box
+      id="project"
+      minH="100vh"
+      backgroundColor=""
+      paddingTop="80px"
+      paddingX="5vw"
+    >
       <Grid templateColumns="repeat(2,1fr)">
         <GridItem
           display="flex"
@@ -32,9 +74,19 @@ const ProjectPage = () => {
             maxW="fit-content"
             marginRight="40px"
           >
-            <Box display="flex" flexDirection="column" gap="4" maxW="450px" paddingY="15px">
+            <Box
+              display="flex"
+              flexDirection="column"
+              gap="4"
+              maxW="450px"
+              paddingY="15px"
+            >
               <Heading size="md">Project Show Case</Heading>
-              <Text fontSize="xl">Take a look at the products we&apos;ve recently finished and see case studies for the full story on how we&apos;ve helped our clients achieve success in the digital world.</Text>
+              <Text fontSize="xl">
+                Take a look at the products we&apos;ve recently finished and see
+                case studies for the full story on how we&apos;ve helped our
+                clients achieve success in the digital world.
+              </Text>
             </Box>
             {getProject(icons)[0]}
           </Box>
@@ -52,7 +104,7 @@ const ProjectPage = () => {
             maxW="fit-content"
             marginLeft="40px"
           >
-            {getProject(icons)[0]}
+            {getProject(icons)[1]}
           </Box>
         </GridItem>
       </Grid>
@@ -64,8 +116,22 @@ const getProject = (projects) => {
   let content2 = [];
   for (let i = 0; i < projects.length; i++) {
     i % 2 === 0
-      ? content1.push(<CardProject key={i} iconData={projects[i]['icons']}/>)
-      : content2.push(<CardProject key={i} iconData={projects[i]['icons']}/>);
+      ? content1.push(
+          <CardProject
+            key={i}
+            title={projects[i]["title"]}
+            desc={projects[i]["desc"]}
+            iconData={projects[i]["icons"]}
+          />
+        )
+      : content2.push(
+          <CardProject
+            key={i}
+            title={projects[i]["title"]}
+            desc={projects[i]["desc"]}
+            iconData={projects[i]["icons"]}
+          />
+        );
   }
   return [content1, content2];
 };
@@ -80,31 +146,38 @@ const CardProject = (props) => {
         alt="image"
       />
       <Box padding="20px" display="flex" flexDirection="column" gap="1">
-        <Heading size="md">Project ShowCase</Heading>
-        <Text>hello</Text>
-        <Text fontSize="xl">(xl) In love with React & Next</Text>
+        <Heading size="md">{props.title}</Heading>
+        <Text fontSize="xl">{props.desc}</Text>
         <Box display="flex" justifyContent="end" gap="0">
-          {props.iconData.map((item,key)=>(
-
-                <IconButton
-                key={key}
-            colorScheme="black"
-            variant="ghost"
-            icon={item}
-            _hover={{
-              bg: "#F0F0F0"
-            }}
-            _active={{
-              bg: "#E0E0E0"
-            }}
-            _focus={{
-              boxShadow: "0 0 0 0 transparent"
-            }}
-          />
+          {props.iconData.map((item, key) => (
+            <CustomIconButton key={key} item={item} />
           ))}
         </Box>
       </Box>
     </Box>
+  );
+};
+const CustomIconButton = (props) => {
+  return (
+    <IconButton
+      onclick={() => {
+        e.preventDefault();
+        window.location.href = "http://google.com";
+      }}
+      // key={key}
+      colorScheme="black"
+      variant="ghost"
+      icon={props.item}
+      _hover={{
+        bg: "#F0F0F0",
+      }}
+      _active={{
+        bg: "#E0E0E0",
+      }}
+      _focus={{
+        boxShadow: "0 0 0 0 transparent",
+      }}
+    />
   );
 };
 export default ProjectPage;
